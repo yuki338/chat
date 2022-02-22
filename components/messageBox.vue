80db1c9be49b259808c3fc35281319d689510367
yuki <glansenkomet@icloud.com>
2022-02-22 20:49:30 +0900

    テキストボックス追加

---
+++ components/messageBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="input"
+      clearable
+      :append-outer-icon="'mdi-send'"
+    ></v-text-field>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'messageBox',
+  data() {
+    return {
+      input: 'test'
+    }
+  }
+}
+</script>
