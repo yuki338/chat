65b2775ca2b77a27756751f1b07544f576acd62e
yuki <glansenkomet@icloud.com>
2022-03-26 22:17:46 +0900

    メッセージ表示を追加

---
+++ components/MessageView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-card-text v-for="message in messages" :key="message.id">
+      {{message.message}}
+    </v-card-text>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MessageStore } from '~/store'
+
+export default Vue.extend({
+  name: 'MessageView',
+  computed: {
+    messages: function () {
+      return MessageStore.getMessages
+    } 
+  }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
+      <message-view></message-view>
       <message-input></message-input>
     </v-col>
   </v-row>
@@ -8,8 +9,9 @@
 
 <script lang="ts">
 import MessageInput from '~/components/MessageInput.vue'
+import MessageView from '~/components/MessageView.vue'
 export default {
-  components: { MessageInput },
+  components: { MessageInput, MessageView },
   name: 'IndexPage',
 }
 </script>
