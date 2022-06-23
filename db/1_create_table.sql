15f6463196e166ad67e2284f75f215577c686bc6
yuki <glansenkomet@icloud.com>
2022-06-23 22:18:05 +0900

    コメント追加

--- db/1_create_table.sql
+++ db/1_create_table.sql
@@ -3,3 +3,6 @@ CREATE TABLE `test` (
   `text` text,
   PRIMARY KEY (`testId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- chatユーザーのパスワードを再設定すること
+-- alter user chat identified with mysql_native_password by '';
