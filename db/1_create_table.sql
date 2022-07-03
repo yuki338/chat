3e69e2ee0d8a1ecec01c354c20543b75d2298e9b
yuki <glansenkomet@icloud.com>
2022-07-03 18:29:39 +0900

    マイグレーション修正

--- db/1_create_table.sql
+++ db/1_create_table.sql
@@ -1,8 +1,31 @@
-CREATE TABLE `test` (
-  `testId` int NOT NULL AUTO_INCREMENT,
-  `text` text,
-  PRIMARY KEY (`testId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- 削除処理
+-- drop table if exists room;
+-- drop table if exists message;
+-- drop table if exists user;
+
+create table `room` (
+  `roomId` varchar(16) not null,
+  `deleteFlg` tinyint default 0 not null,
+  primary key (`roomId`)
+);
+
+create table `message` (
+  `messageId` int not null auto_increment,
+  `roomId` varchar(16) default '',
+  `message` text not null,
+  `userId` int default 0 not null,
+  `dateTime` datetime not null,
+  `deleteFlg` tinyint default 0 not null,
+  primary key (`messageId`),
+  index idxMessageId (`roomId`)
+);
+
+create table `user` (
+  `userId` int not null auto_increment,
+  `name` varchar(32) default '' not null,
+  `deleteFlg` tinyint default 0 not null,
+  primary key (`userId`)
+);
 
 -- chatユーザーのパスワードを再設定すること
 -- alter user chat identified with mysql_native_password by '';
